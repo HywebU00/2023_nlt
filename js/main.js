366d8b76b879f193c0c7a16afb02c2fca58f7ced
Miley <miley.ho@hyweb.com.tw>
2023-10-18 15:26:56 +0800

    調整category

--- js/main.js
+++ js/main.js
@@ -1290,7 +1290,7 @@ function categoryActive() {
     const item = i.querySelectorAll('a');
     item.forEach((tag) => {
       tag.addEventListener('click', (e) => {
-        e.preventDefault();
+        //e.preventDefault();
         removeClass(item);
         e.target.classList.add('active');
       });
